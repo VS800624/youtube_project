5c99c49e9934a1ed1e1cbfdcee9a2568907f09db
VS800624 <vishalsingh800624@gmail.com>
2025-09-07 12:51:31 +0530

    Initial youtube project files with Tailwind and Redux setup

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+const GOOGLE_API_KEY = "AIzaSyCJEyxyGlfkjbhA5hA1rFPURZmD-Fbj0g0"
+
+
+export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=50&key="+ GOOGLE_API_KEY;
